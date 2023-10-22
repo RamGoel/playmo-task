30221c68e989e27e8558c7190bed13207a08c03f
Ram Goel <rgoel766@gmail.com>
2023-10-23 00:55:28 +0530

    lint fixes

--- app/page.tsx
+++ app/page.tsx
@@ -30,53 +30,53 @@ const Form = () => {
   return (
     <div className='main-form h-screen w-screen flex items-center justify-center'>
       <div className='flex flex-col w-11/12 md:w-1/2 lg:w-1/3'>
-        <Question question={questionsData[index].question} />
+        <Question question={questionsData?.[index]?.question} />
         <div className='flex flex-col'>
           {
-            questionsData[index].choices ? questionsData[index]?.choices?.map((option, i) => {
+            questionsData?.[index]?.choices ? questionsData?.[index]?.choices?.map((option, i) => {
               return (
                 <Option
                   key={i}
                   text={option}
                   handler={() => {
-                    if (questionsData[index].questionType === 'single-choice') {
-                      setFormData({ ...formData, [questionsData[index].dataKey]: option })
+                    if (questionsData?.[index]?.questionType === 'single-choice') {
+                      setFormData({ ...formData, [questionsData?.[index]?.dataKey]: option })
                       return;
                     } else {
-                      if (!formData[questionsData[index].dataKey]) {
+                      if (!formData?.[questionsData?.[index]?.dataKey]) {
                         setFormData({
                           ...formData,
-                          [questionsData[index].dataKey]: [option]
+                          [questionsData?.[index]?.dataKey]: [option]
                         })
                       } else {
                         
                         setFormData({
                           ...formData,
-                          [questionsData[index].dataKey] : [...formData[questionsData[index].dataKey], option]
+                          [questionsData?.[index]?.dataKey] : [...formData?.[questionsData?.[index]?.dataKey], option]
                         })
                         return;
                       }
                     }
                   }}
-                  isSelected={formData[questionsData[index].dataKey]?.includes(option)}
+                  isSelected={formData?.[questionsData?.[index]?.dataKey]?.includes(option)}
                 />
               )
             }) : <Input
-              value={formData[questionsData[index].dataKey]}
-              handler={(val) => setFormData({ ...formData, [questionsData[index].dataKey]: val })}
-              inputType={questionsData[index].inputType}
-              placeholder={questionsData[index].placeholder}
+              value={formData?.[questionsData?.[index]?.dataKey]}
+              handler={(val) => setFormData({ ...formData, [questionsData?.[index]?.dataKey]: val })}
+              inputType={questionsData?.[index]?.inputType}
+              placeholder={questionsData?.[index]?.placeholder}
             />
           }
 
           <div className='flex items-center justify-between'>
 
             <Button arrowText='←' handler={() => {
-              setIndex(questionsData[index].prevQuestionIndex())
+              setIndex(questionsData?.[index]?.prevQuestionIndex())
             }} isDisabled={index === 0} />
             <p className='text-sm transition-all text-red-600 error-message'>{error}</p>
             <Button arrowText='→' handler={() => {
-              if (!formData[questionsData[index].dataKey]) {
+              if (!formData?.[questionsData?.[index]?.dataKey]) {
                 if (index === 0) {
                   setError('Please enter date of birth')
                 } else {
@@ -84,7 +84,7 @@ const Form = () => {
                 }
                 return
               }
-              const val = questionsData[index].nextQuestionIndex(formData[questionsData[index].dataKey])
+              const val = questionsData?.[index]?.nextQuestionIndex(formData?.[questionsData?.[index]?.dataKey])
               if (val !== -1 && val !== undefined) {
                 setIndex(val)
               } else {
